f525b58ad39d0d4f4219fb2686ca50f2faf01df6
David Gnanasekaran <david_sekar@msn.com>
2019-04-18 23:23:08 +0530

    Service worker route updates

--- docs/sw.js
+++ docs/sw.js
@@ -6,12 +6,28 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
 // Cache the fontawesome fonts.
 workbox.routing.registerRoute(
     /^https:\/\/netdna\.bootstrapcdn\.com\/font-awesome\/4\.7\.0\//,
-    new workbox.strategies.StaleWhileRevalidate({
+    new workbox.strategies.CacheFirst({
         cacheName: 'fontawesome-fonts-stylesheets',
         plugins: [
             new workbox.expiration.Plugin({
                 maxEntries: 20,
             }),
+        ]
+    }),
+);
+
+// Cache the site images.
+workbox.routing.registerRoute(
+    /\/img\/*/i,
+    new workbox.strategies.CacheFirst({
+        cacheName: 'pwa-sample-images',
+        plugins: [
+            new workbox.expiration.Plugin({
+                // Only cache requests for a week
+                maxAgeSeconds: 7 * 24 * 60 * 60,
+                // Only cache 10 requests.
+                maxEntries: 10
+            })
         ]
     }),
 );
